efab3feb83e22098df0891002fd8fc435ff1fa0c
kaing <hongweikang@gmail.com>
2017-02-23 17:16:28 +0800

    Add sixth.

--- src/main/java/com/kaing/algorithm/leetcode/Test.java
+++ src/main/java/com/kaing/algorithm/leetcode/Test.java
@@ -7,10 +7,24 @@ package com.kaing.algorithm.leetcode;
  */
 public class Test {
     public static void main(String[] args) {
-        System.out.println(0/2);
-        System.out.println(1/2);
-        System.out.println(2/2);
-        System.out.println(3/2);
-        System.out.println(4/2);
+        System.out.println(0 / 2);
+        System.out.println(1 / 2);
+        System.out.println(2 / 2);
+        System.out.println(3 / 2);
+        System.out.println(4 / 2);
+
+        System.out.println(30 / 4);
+        System.out.println(30 % 4);
+        System.out.println(3 / 4);
+        System.out.println(3 % 4);
+        char[] a = new char[5];
+        a[0] = 'x';
+        a[1] = 'y';
+        a[2] = 'z';
+        StringBuffer b = new StringBuffer();
+        b.append(a);
+        String s = b.toString();
+        s = s.replaceAll("0", "");
+        System.out.println(s);
     }
 }
