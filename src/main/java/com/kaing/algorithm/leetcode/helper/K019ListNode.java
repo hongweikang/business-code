6772a3e5af9c76f71181d1a9e5eae18bbb619049
kaing <hongweikang@gmail.com>
2017-03-06 18:08:16 +0800

    The 18, 19.

---
+++ src/main/java/com/kaing/algorithm/leetcode/helper/K019ListNode.java
@@ -0,0 +1,18 @@
+package com.kaing.algorithm.leetcode.helper;
+
+/**
+ * User: hongweikang
+ * Date: 06/03/2017
+ * Time: 2:46 PM
+ */
+public class K019ListNode {
+
+    private int val;
+
+    public K019ListNode next;
+
+    public K019ListNode(int x) {
+        val = x;
+    }
+
+}
