b9d3a630dfa5f0e52ca5440bba730ceb551b1a6f
kaing <hongweikang@gmail.com>
2017-04-04 13:54:04 +0800

    39th.

--- src/main/java/com/kaing/algorithm/kmp/AAA.java
+++ src/main/java/com/kaing/algorithm/kmp/AAA.java
@@ -8,4 +8,6 @@ package com.kaing.algorithm.kmp;
 public class AAA {
 // KMP算法
 //    http://blog.csdn.net/yutianzuijin/article/details/11954939/
+//    http://blog.csdn.net/joylnwang/article/details/6778316/
+//    http://blog.csdn.net/power721/article/details/6132380
 }
